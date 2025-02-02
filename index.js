8b31517779e6b9931482935ae0a93a2948eff889
NEXTGENGAMING <xxphoeni@icloud.com>
2025-02-02 07:12:05 +0000

    nav bar adjustments

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ function toggleSearch() {
     if (searchBar.classList.contains('active')) {
       searchBar.focus();
     }
+}
+function toggleNav() {
+  var sidenav = document.getElementById("mySidenav");
+  if (sidenav.classList.contains("open")) {
+      sidenav.classList.remove("open");
+  } else {
+      sidenav.classList.add("open");
   }
-  
+}
